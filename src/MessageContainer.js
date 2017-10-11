6cae488ff43a2a549e384be4e812a7a063c18dd9
maks_ <maks_@intersvyaz.net>
2017-10-11 10:43:24 +0500

    Fix chat history for huyawei

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {
   ListView,
   View,
+  StyleSheet,
+  Platform
 } from 'react-native';
 
 import shallowequal from 'shallowequal';
@@ -137,7 +139,7 @@ export default class MessageContainer extends React.Component {
 
   render() {
     return (
-      <View ref='container' style={{flex:1}}>
+      <View ref='container' style={[styles.invertAndroid, {flex: 1}]}>
         <ListView
           enableEmptySections={true}
           automaticallyAdjustContentInsets={false}
@@ -157,6 +159,20 @@ export default class MessageContainer extends React.Component {
     );
   }
 }
+
+let styles = StyleSheet.create({
+  invertAndroid: Platform.select({
+    android: {
+      scaleY: -1,
+      transform: [
+        {
+          scaleY: -1,
+          perspective: 1280
+        },
+      ],
+    },
+  }),
+});
 
 MessageContainer.defaultProps = {
   messages: [],
